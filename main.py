9d3485c17d652190af775dbc7c215308de4230da
waqar <>
2020-04-06 00:32:44 +0500

    updated main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import json
 from bs4 import BeautifulSoup
 
 
-proxies = {"http": "http://127.0.0.1:8080", "https": "http://127.0.0.1:8080"}
+#proxies = {"http": "http://127.0.0.1:8080", "https": "http://127.0.0.1:8080"}
 #r = requests.get("https://www.google.com/", proxies=proxies, verify=False)
-user_agent = "waleed"
+user_agent = "Chrome"
 domain = "easyclass.com"
 
 def post_info(json):
